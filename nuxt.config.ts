5455c48efd3723b1722d3edcac62f2149f7fe23d
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-09-10 19:34:21 +0900

    🎉 init

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  tailwindcss: {
+    cssPath: "~assets/css/tailwind.css",
+    configPath: "tailwind.config.js",
+    exposeConfig: false,
+    config: {},
+    injectPosition: 0,
+    viewer: true,
+  },
+  colorMode: {
+    classSuffix: "",
+  },
+});
